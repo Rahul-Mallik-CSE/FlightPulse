d73d2b8926ef258e474a6ea2b4fba482b3945da5
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-06-19 23:07:31 +0600

    Add flight data and update Next.js configuration for image handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning= {true}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background">
